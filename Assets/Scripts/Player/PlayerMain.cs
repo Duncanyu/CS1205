8a3a169cf730de54e47a3216e0954ae66b21a3d4
Duncanyu <duncan.w.yu@gmail.com>
2025-05-27 10:36:32 -0400

    Implemented basic movement

--- Assets/Scripts/Player/PlayerMain.cs
+++ Assets/Scripts/Player/PlayerMain.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
     }
 }
