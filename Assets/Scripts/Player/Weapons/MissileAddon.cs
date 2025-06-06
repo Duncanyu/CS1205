b43338ccb072c5a39e728126e35684c3f6fddf22
Duncanyu <duncan.w.yu@gmail.com>
2025-06-06 08:00:53 -0400

    Started work on Missile (sorting and searching alg 1)
    This is a makeup for yesterday. There will be another commit today, if not, tomorrow.

---
+++ Assets/Scripts/Player/Weapons/MissileAddon.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MissileAddon : MonoBehaviour
+{
+    public GameObject missilePrefab;
+    public float cooldown = 8f;
+    public float minRange = 3f;
+    public float maxRange = 10f;
+
+    private float lastTriggerTime = -10000000;
+    private Transform playerTransform;
+
+    public void Initialize(Transform player):
+}
