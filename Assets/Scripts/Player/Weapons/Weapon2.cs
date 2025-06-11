a27b8935170896ca68aff538741ea25931a6ef25
Heng2007 <zhuheng2007@gmail.com>
2025-06-11 10:02:34 -0400

    Update Weapon2.cs

--- Assets/Scripts/Player/Weapons/Weapon2.cs
+++ Assets/Scripts/Player/Weapons/Weapon2.cs
@@ -5,21 +5,40 @@ using UnityEngine.UIElements;
 public class Weapon2 : WeaponBase
 {
     public GameObject bulletPrefab;
+    private float _randomFireRate;
+
+    private void Awake()
+    {
+        _randomFireRate = Random.Range(0.1f, 5f); 
+    }
 
     protected override void Shoot(Vector2 direction)
     {
+       
         if (playerTransform == null)
         {
             Debug.LogWarning("no playetreansform set.");
             return;
         }
-        GameObject bullet = Instantiate(bulletPrefab, Vector3, Quaternion rotation, Transform parent);
+        GameObject bullet = Instantiate(bulletPrefab, new Vector3(
+        playerTransform.position.x,
+        playerTransform.position.y - 3,
+        playerTransform.position.z
+    ), Quaternion.identity);
     }
 
-    public override void Fire(Vector2 direction)
-    {
-       
+
+    public override void Fire(Vector2 direction) {
+        if (Time.time >= lastFiredTime + _randomFireRate)
+        {
+
+
+            lastFiredTime = Time.time;
+            Shoot(direction);
+        }
     }
+    
+
 }
  
 
