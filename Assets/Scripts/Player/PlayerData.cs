f4517aa671632cfc0043cd15c4e56924bc78cd29
TonyT1230 <tonyt07123@gmail.com>
2025-06-11 16:12:50 -0400

    Added player saving and loading

---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData {
+    public float[] Position;
+    public float[] moveSpeed;
+    public int Score;
+    public WeaponBase CurrentWeapon; 
+
+}
